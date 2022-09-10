7066bed44a5f978093ab3e9c6966bfc1d3d4c584
pattamuthug <pattamuthu51@gmail.com>
2022-09-10 10:19:04 +0530

    js-math
    js-math

--- main.js
+++ main.js
@@ -1,77 +1,60 @@
-// // Math 
-// // STATIC PROPERTIES
-// // Math.E
-// let eulers = Math.E;
-// console.log("Math.E", eulers);
+// Math 
+// STATIC PROPERTIES
+// Math.E
+let eulers = Math.E;
+console.log("Math.E", eulers);
 
-// // Math.LN2
-// let log = Math.LN2;
-// console.log('Math.LN2', log);
+// Math.LN2
+let log = Math.LN2;
+console.log('Math.LN2', log);
 
-// // Math.LN10
-// let log10 = Math.LN10;
-// console.log('Math.LN10', log10);
+// Math.LN10
+let log10 = Math.LN10;
+console.log('Math.LN10', log10);
 
-// // Math.LOG2E
-// let a = Math.LOG2E;
-// console.log('Math.LOG2E', a);
+// Math.LOG2E
+let a = Math.LOG2E;
+console.log('Math.LOG2E', a);
 
-// // Math.LOG10E
-// let b = Math.LOG10E;
-// console.log('Math.LOG10E', b);
+// Math.LOG10E
+let b = Math.LOG10E;
+console.log('Math.LOG10E', b);
 
-// // Math.PI
-// let c = Math.PI;
-// console.log('Math.PI', c);
+// Math.PI
+let c = Math.PI;
+console.log('Math.PI', c);
 
-// // Math.SQRT1_2
-// let d = Math.SQRT1_2;
-// console.log(d);
+// Math.SQRT1_2
+let d = Math.SQRT1_2;
+console.log(d);
 
-// // Math.SQRT2
-// let e = Math.SQRT2;
-// console.log('Math.SQRT2', e);
+// Math.SQRT2
+let e = Math.SQRT2;
+console.log('Math.SQRT2', e);
 
-// // STATIC METHODS
+// STATIC METHODS
 
-// // Math.abs()
-// let f = Math.abs(-5); g = Math.abs(0); h = Math.abs('string'); i = Math.abs(7);
-// console.log('-5', f);
-// console.log('0', g);
-// console.log('string', h);
-// console.log('7', i);
+// Math.abs()
+let f = Math.abs(-5); g = Math.abs(0); h = Math.abs('string'); i = Math.abs(7);
+console.log('-5', f);
+console.log('0', g);
+console.log('string', h);
+console.log('7', i);
 
-// // Math.cbrt()
-// let j = Math.cbrt(8); k = Math.cbrt(-125);
-// console.log('8', j);
-// console.log('-125', k);
+// Math.cbrt()
+let j = Math.cbrt(8); k = Math.cbrt(-125);
+console.log('8', j);
+console.log('-125', k);
 
-// // Math.ceil()
-// let l = Math.ceil(1.2); m = Math.ceil(-1.2);
-// console.log("1.2", l);
-// console.log("-1.2", m);
+// Math.ceil()
+let l = Math.ceil(1.2); m = Math.ceil(-1.2);
+console.log("1.2", l);
+console.log("-1.2", m);
 
-// // Math.cos()
-// let n=Math.cos(10);
-// console.log('10',n);
+// Math.cos()
+let n=Math.cos(10);
+console.log('10',n);
 
-// // Math.exp()
-// let o=Math.exp(1);
-// console.log('1',o);
-
-var btn = document.getElementById("btn");
-var siva = document.getElementById("siva");
-
-
-
-var color=()=>{
-        var x = Math.floor(Math.random() * 256);
-        var y = Math.floor(Math.random() * 256);
-        var z = Math.floor(Math.random() * 256);
-        var ranclr = "rgb("+ x + "," + y + "," + z + ")";
-     console.log(ranclr);
-      
-        siva.style.color = ranclr;
-        
-}
-btn.addEventListener("click",color);
+// Math.exp()
+let o=Math.exp(1);
+console.log('1',o);
